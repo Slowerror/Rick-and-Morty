1197a9099cc37b3eedad069fec7177dbfb9cffa7
Nikolay Zetkin <nszetkin76@gmail.com>
2024-04-21 20:14:54 +0300

    Исправил ошибку в SearchCharacterDiffUtil

--- app/src/main/java/com/slowerror/rickandmorty/ui/search_character_list/SearchCharacterListAdapter.kt
+++ app/src/main/java/com/slowerror/rickandmorty/ui/search_character_list/SearchCharacterListAdapter.kt
@@ -51,7 +51,7 @@ class SearchCharacterListAdapter(
 
     object SearchCharacterDiffUtil : DiffUtil.ItemCallback<Character>() {
         override fun areItemsTheSame(oldItem: Character, newItem: Character): Boolean {
-            return oldItem.name == newItem.name
+            return oldItem.id == newItem.id
         }
 
         override fun areContentsTheSame(oldItem: Character, newItem: Character): Boolean {
